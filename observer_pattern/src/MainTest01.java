f1b4e0c0095798b95b0b11c463ee6e1df5cf831f
shlstart <timothylei12@gmail.com>
2022-07-12 18:40:17 -0400

    hot-fix commit1

--- observer_pattern/src/MainTest01.java
+++ observer_pattern/src/MainTest01.java
@@ -8,6 +8,7 @@ public class MainTest01 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello github");
+        System.out.println("hello git hot-fix");
     }
 }
 
