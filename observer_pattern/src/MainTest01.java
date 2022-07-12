13f9b91a07b011a857494e6c4f5e13cce36274c9
shlstart <timothylei12@gmail.com>
2022-07-12 18:02:04 -0400

    first commit

---
+++ observer_pattern/src/MainTest01.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+/**
+ * Author: shlstart
+ * Create: 2022-07-10
+ */
+public class MainTest01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
+
+
+
+abstract class Subject {
+
+    // a collections of observers
+    protected ArrayList<Observer> observers = new ArrayList<>();
+
+    // add an observer to list
+    public void attach (Observer observer) {
+        observers.add(observer);
+    }
+
+    // remove an observer from list
+    public void detach (Observer observer) {
+        observers.remove(observer);
+    }
+    // let its subclass to implement this method to notify its ally
+    public abstract void notifyObserver();
+}
+
+class ConcreteMainTest01 extends Subject {
+
+    // notify its ally
+    @Override
+    public void notifyObserver() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+
+    }
+}
+
+
+interface Observer {
+    public void update();
+}
+
+class ConcreteObserver implements Observer {
+
+    @Override
+    public void update() {
+
+    }
+}
